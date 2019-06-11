980d7f0be8d6aaf1df45c9eebc4c60405c9ee257
AlexandrNedilko <aleksandr_ned@ukr.net>
2019-06-11 21:12:58 +0300

    Add Task1

---
+++ src/main/java/annotation/Test.java
@@ -0,0 +1,10 @@
+package annotation;
+import java.lang.annotation.*;
+
+@Inherited
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public @interface Test {
+    int a();
+    int b();
+}
